c7ab03532c240893328e65f81823ca56dfd93c52
lpeller <louis.peller@orange.fr>
2023-02-19 19:47:53 +0100

    ajout de l'échelle sur la carte

---
+++ web/js/echelle.js
@@ -0,0 +1,45 @@
+const scale_eu = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            14.8,
+            41
+          ],
+          [
+            12.9982,
+            41
+          ]
+        ],
+        "type": "LineString"
+      }
+    }
+  ]
+};
+
+const scale_fr = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            7.8,
+            42.8
+          ],
+          [
+            5.9982,
+            42.8
+          ]
+        ],
+        "type": "LineString"
+      }
+    }
+  ]
+};
